b5cc8d015db7dbf0802d4068e5320e4edff45438
Eparth25 <htrap1211@gmail.com>
2025-12-26 13:55:51 +0530

    Initial Commit

---
+++ sniper/sniperApp.swift
@@ -0,0 +1,17 @@
+//
+//  sniperApp.swift
+//  sniper
+//
+//  Created by Parth Rathi on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct sniperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
